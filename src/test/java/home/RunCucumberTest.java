fbce9f8482d38f0147e862c75606e52a2c443aca
cat2788 <catrin2788@gmail.com>
2019-09-15 16:37:12 +0100

    First commit

---
+++ src/test/java/home/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package home;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+}
